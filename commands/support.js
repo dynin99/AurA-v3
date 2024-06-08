4e2b9145f594e0906f6831b19db81d4c8576d70b
dynin99 <163668096+dynin99@users.noreply.github.com>
2024-06-08 16:10:30 +0530

    support.js

--- commands/support.js
+++ commands/support.js
@@ -1,23 +1,4 @@
-/*
 
-  ________.__                        _____.___.___________
- /  _____/|  | _____    ____  ____   \__  |   |\__    ___/
-/   \  ___|  | \__  \ _/ ___\/ __ \   /   |   |  |    |   
-\    \_\  \  |__/ __ \\  \__\  ___/   \____   |  |    |   
- \______  /____(____  /\___  >___  >  / ______|  |____|   
-        \/          \/     \/    \/   \/                  
-
-╔════════════════════════════════════════════════════════════════════════╗
-║                                                                        ║
-║  ## Created by GlaceYT!                                                ║
-║  ## Feel free to utilize any portion of the code                       ║
-║  ## DISCORD :  https://discord.com/invite/xQF9f9yUEM                   ║
-║  ## YouTube : https://www.youtube.com/@GlaceYt                         ║
-║                                                                        ║
-╚════════════════════════════════════════════════════════════════════════╝
-
-
-*/
 
 const { EmbedBuilder } = require('discord.js');
 
@@ -29,10 +10,9 @@ module.exports = {
   run: async (client, interaction) => {
     try {
 
-      const supportServerLink = "https://discord.gg/xQF9f9yUEM";
-      const githubLink = "https://github.com/GlaceYT";
-      const replitLink = "https://replit.com/@GlaceYT";
-      const youtubeLink = "https://www.youtube.com/@GlaceYT";
+      const supportServerLink = "https://dsc.gg/bulsharky";
+      const instaLink = "https://www.instagram.com/ig_bulsharky/";
+      const youtubeLink = "https://www.youtube.com/@BulsharkyisLive";
         const embed = new EmbedBuilder()
             .setColor('#b300ff')
             .setAuthor({
@@ -40,8 +20,8 @@ module.exports = {
               iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230824519220985896/6280-2.gif?ex=6638ae28&is=66375ca8&hm=13e4a1b91a95b2934a39de1876e66c11711c7b30ac1a91c2a158f2f2ed1c2fc6&', 
               url: 'https://discord.gg/xQF9f9yUEM'
           })
-            .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n➡️ **Follow us on:**\n- GitHub - ${githubLink}\n- Replit - ${replitLink}\n- YouTube - ${youtubeLink}`)
-            .setImage('https://cdn.discordapp.com/attachments/1113800537402527903/1236803979996958740/11.png?ex=663956f7&is=66380577&hm=3b3c19a11adcb979517a133f2907f671305d23f1f5092cf7df043e6d5cab07bc&')
+            .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n➡️ **Follow us on:**\n- Instagram - ${instaLink}\n- YouTube - ${youtubeLink}`)
+            .setImage('https://cdn.discordapp.com/attachments/1094553235022417953/1248949502937796699/e02acbbca19e929041e66d65edf835ca_2.jpg?ex=6665865d&is=666434dd&hm=d8ebcda1780a6b60802a1286de0e528f42c71c1926fc2ae02e4782f2c792ba82&')
             .setTimestamp();
 
       return interaction.reply({ embeds: [embed] });
